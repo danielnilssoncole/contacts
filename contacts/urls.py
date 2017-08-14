8b3aac9a78f1a3907792bb204c0834c52355bb3d
daniel <dnilssoncole@gmail.com>
2017-08-14 07:15:33 -0400

    contact detail page

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from contacts import views
 
+app_name = 'contacts'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<contact_id>[0-9]+)/$',
+        views.show_contact, name='show_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contacts.models import Contact
 
 def index(request):
     contact_list = Contact.objects.order_by('-created_at')[:5]
     context = {'contacts': contact_list}
     return render(request, 'contacts/index.html', context=context)
+
+def show_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    context = {'contact': contact,
+               'full_name': contact.full_name,
+               'phone_number': contact.phone_number,
+               'email': contact.email,
+               'created_at': contact.created_at,
+               'updated_at': contact.updated_at}
+    return render(request, 'contacts/contact.html', context)
